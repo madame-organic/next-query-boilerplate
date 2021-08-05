e79fd4e3bd49784875a63f5625b4243531d6684e
Bivek Singh <beevk.singh@gmail.com>
2021-08-05 17:23:38 +0700

    feat: configure google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext, Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
@@ -6,16 +6,33 @@ class MyDocument extends Document {
     }
 
     render(): JSX.Element {
+        const trackingId = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
         return (
             <Html>
                 <Head>
                     <title>Madame Organic: MO subtitle</title>
                     <meta charSet="UTF-8" />
                     <meta name="description" content="Madame Organic is sth..." />
-                    <meta name="keywords" content="Madame Organic, beauty soap, foundation, makeup" />
+                    <meta
+                        name="keywords"
+                        content="Madame Organic, beauty soap, foundation, makeup"
+                    />
                     <meta name="author" content="Bchu Dev" />
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <link rel="shortcut icon" href="/favicon.ico" />
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${trackingId}', {page_path: window.location.pathname});`,
+                        }}
+                    />
                 </Head>
                 <body className="body">
                     <Main />
